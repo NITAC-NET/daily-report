63fff5734143be767947a4e1fa4e632a54862170
NanyaTK <herenfuyong02@gmail.com>
2024-02-03 23:53:28 +0900

    feat: test

--- test.py
+++ test.py
@@ -7,8 +7,10 @@ API_KEY = os.getenv("TWITTER_API_KEY")
 API_SECRET = os.getenv("TWITTER_API_SECRET_KEY")
 ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 client = tweepy.Client(
+    bearer_token=BEARER_TOKEN,
     consumer_key=API_KEY,
     consumer_secret=API_SECRET,
     access_token=ACCESS_TOKEN,
